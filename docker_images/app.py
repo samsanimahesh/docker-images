e65fcb980b62dad2f304f93e1d213669102ef6dc
samsanimahesh@gmail.com <mbsamsani@ucdavis.edu>
2018-09-01 21:15:50 +0530

    updated folders

--- docker_images/app.py
+++ docker_images/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized version 2 from hostname '+socket.gethostname()
+    return 'Flask Dockerized version 2'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
