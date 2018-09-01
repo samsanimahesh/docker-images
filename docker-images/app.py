a33521caff83eaff5d5274d9d09e98ab2474913f
samsanimahesh@gmail.com <mbsamsani@ucdavis.edu>
2018-09-01 17:45:39 +0530

    added docker images and pod example

--- docker-images/app.py
+++ docker-images/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized version 2'
+    return 'Flask Dockerized version 2 from hostname '+socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
